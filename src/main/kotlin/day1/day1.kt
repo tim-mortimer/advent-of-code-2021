a19c6b392ca90d4beb137ec3c05f5b8f19554efa
Tim Mortimer <tim@kiteframe.co.uk>
2023-08-05 19:17:05 +0100

    Day 1 - refactor

--- src/main/kotlin/day1/day1.kt
+++ src/main/kotlin/day1/day1.kt
@@ -4,12 +4,12 @@ import java.io.File
 
 fun main() {
     fun part1(): Int {
-        val input = File("src/main/kotlin/day1/part1input.txt").readLines().map { it.toInt() }
+        val input = File("src/main/kotlin/day1/input.txt").readLines().map { it.toInt() }
         return input.windowed(2).count { (a, b) -> b > a }
     }
 
     fun part2(): Int {
-        val input = File("src/main/kotlin/day1/part2input.txt").readLines().map { it.toInt() }
+        val input = File("src/main/kotlin/day1/input.txt").readLines().map { it.toInt() }
         return input.windowed(3)
             .map { a -> a.sum() }
             .windowed(2)
