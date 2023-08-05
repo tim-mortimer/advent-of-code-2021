260746a3cd53fecf03e976e7328e6df96249cd22
Tim Mortimer <tim@kiteframe.co.uk>
2023-08-05 22:17:07 +0100

    Utilise copy function

--- src/main/kotlin/day2/day2.kt
+++ src/main/kotlin/day2/day2.kt
@@ -7,21 +7,19 @@ fun main() {
     data class Position(var horizontal: Int, var vertical: Int)
 
     fun getInstructions(): List<Instruction> {
-        return readLinesInFile("day2/input.txt")
-            .map { line -> line.split(" ") }
+        return readLinesInFile("day2/input.txt").map { line -> line.split(" ") }
             .map { split -> Instruction(split[0], split[1].toInt()) }
     }
 
     fun part1(): Int {
-        val endPosition = getInstructions()
-            .fold(Position(0, 0)) { position, instruction ->
-                when (instruction.command) {
-                    "forward" -> Position(position.horizontal + instruction.amount, position.vertical)
-                    "up" -> Position(position.horizontal, position.vertical - instruction.amount)
-                    "down" -> Position(position.horizontal, position.vertical + instruction.amount)
-                    else -> throw IllegalArgumentException("Command must be one of: forward, up, down")
-                }
+        val endPosition = getInstructions().fold(Position(0, 0)) { position, instruction ->
+            when (instruction.command) {
+                "forward" -> position.copy(horizontal = position.horizontal + instruction.amount)
+                "up" -> position.copy(vertical = position.vertical - instruction.amount)
+                "down" -> position.copy(vertical = position.vertical + instruction.amount)
+                else -> throw IllegalArgumentException("Command must be one of: forward, up, down")
             }
+        }
 
         return endPosition.horizontal * endPosition.vertical
     }
@@ -29,18 +27,18 @@ fun main() {
     fun part2(): Int {
         data class PositionAndAim(var position: Position, var aim: Int)
 
-        val endPositionAndAim = getInstructions()
-            .fold(PositionAndAim(Position(0, 0), 0)) { positionAndAim, instruction ->
+        val endPositionAndAim =
+            getInstructions().fold(PositionAndAim(Position(0, 0), 0)) { positionAndAim, instruction ->
                 when (instruction.command) {
-                    "forward" -> PositionAndAim(
-                        Position(
+                    "forward" -> positionAndAim.copy(
+                        position = Position(
                             positionAndAim.position.horizontal + instruction.amount,
                             positionAndAim.position.vertical + positionAndAim.aim * instruction.amount
-                        ), positionAndAim.aim
+                        )
                     )
 
-                    "up" -> PositionAndAim(positionAndAim.position, positionAndAim.aim - instruction.amount)
-                    "down" -> PositionAndAim(positionAndAim.position, positionAndAim.aim + instruction.amount)
+                    "up" -> positionAndAim.copy(aim = positionAndAim.aim - instruction.amount)
+                    "down" -> positionAndAim.copy(aim = positionAndAim.aim + instruction.amount)
                     else -> throw IllegalArgumentException("Command must be one of: forward, up, down")
                 }
             }
