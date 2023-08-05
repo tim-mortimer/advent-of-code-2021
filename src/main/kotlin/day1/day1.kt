d519293aa2831ac542d5d629d7cc3fd96e0e6262
Tim Mortimer <tim@kiteframe.co.uk>
2023-08-05 19:16:25 +0100

    Day 1 - part 2

--- src/main/kotlin/day1/day1.kt
+++ src/main/kotlin/day1/day1.kt
@@ -8,5 +8,14 @@ fun main() {
         return input.windowed(2).count { (a, b) -> b > a }
     }
 
+    fun part2(): Int {
+        val input = File("src/main/kotlin/day1/part2input.txt").readLines().map { it.toInt() }
+        return input.windowed(3)
+            .map { a -> a.sum() }
+            .windowed(2)
+            .count { (a, b) -> b > a }
+    }
+
     println(part1())
+    println(part2())
 }
